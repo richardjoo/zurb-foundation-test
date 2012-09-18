ae2ca26116e442665266bf0d9bf543d478652f73
Keil Miller Jr <tuscanidream@me.com>
2012-09-18 19:35:24 -0400

    My first commit
    Signed-off-by: Keil Miller Jr <tuscanidream@me.com>

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+	layout 'plain', :only => [:index]
+
+  def index
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZurbFoundationTest::Application.config.secret_token = 'cc1180d8c23786a628c6312345f7775b2743a2c666f8a3b1cf49be47e58b93c8c47ec39d992d79fd7cde878f7e49db315156e6e5663a1eab833c95d79a8f61f3'
